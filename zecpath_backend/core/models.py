b564d053aa63252f72d1557a61d66cd51a784bec
Adhithyan9778 <kadhithyan903@gmail.com>
2026-09-06 12:50:09 +0530

    Complete Day 4 Django ORM and models

--- zecpath_backend/core/models.py
+++ zecpath_backend/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+
+class Application(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    applied_at = models.DateTimeField(auto_now_add=True)
